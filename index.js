782ce9ca0907eae55522545a0f5cf77963b4854d
Priscillah Njai <priscillahnjaii@gmail.com>
2025-03-26 17:48:19 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+
+  document.addEventListener("DOMContentLoaded", function() {
+    // Log something if you like, just to confirm it ran
+    console.log("The DOM has loaded!");
+  
+    // Grab the paragraph element by its id
+    const textPara = document.getElementById("text");
+  
+    // Update its text
+    textPara.textContent = "This is really cool!";
+  });
+  
